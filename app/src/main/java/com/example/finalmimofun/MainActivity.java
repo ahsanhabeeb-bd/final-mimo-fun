5eb887e825ca73c0df4544609591bfa473532d4c
mimofun <newlight12210@gmail.com>
2022-11-20 19:05:16 +0600

    mail and password design complete

--- app/src/main/java/com/example/finalmimofun/MainActivity.java
+++ app/src/main/java/com/example/finalmimofun/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.finalmimofun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button sign_phone;
+    private Button sign_mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sign_phone =findViewById(R.id.sign_phone);
+        sign_mail =findViewById(R.id.sign_mail);
+
+        sign_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Log_phone_Activity.class));
+
+            }
+        });
+
+        sign_mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Login_mail_Activity.class));
+            }
+        });
+
+
+
     }
+
+
 }
